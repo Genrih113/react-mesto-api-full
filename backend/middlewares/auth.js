6712ae4504058b5256353375859dcd841876b949
Gennadij Tolstov <genrih113@yandex.ru>
2021-04-01 10:01:51 +0300

    отменяет передачу пароля из бэка, вносит изменения в api фронта

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,16 +1,10 @@
 const jwt = require('jsonwebtoken');
 
-const { setCustomErrorStatusAndMessage } = require('../helpers/error-handling-helpers');
-
-const { NODE_ENV, JWT_SECRET } = process.env;
-
-const tokenSignatureKey = (NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
-
-// const handleAuthError = (res) => {
-//   res
-//     .status(401)
-//     .send({ message: 'Необходима авторизация' });
-// };
+const handleAuthError = (res) => {
+  res
+    .status(401)
+    .send({ message: 'Необходима авторизация' });
+};
 
 const extractBearerToken = (header) => header.replace('Bearer ', '');
 
@@ -18,16 +12,16 @@ module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return setCustomErrorStatusAndMessage(401, 'Необходима авторизация');
+    return handleAuthError(res);
   }
 
   const token = extractBearerToken(authorization);
   let payload;
 
   try {
-    payload = jwt.verify(token, tokenSignatureKey);
+    payload = jwt.verify(token, 'some-secret-key');
   } catch (err) {
-    return setCustomErrorStatusAndMessage(401, 'Необходима авторизация');
+    return handleAuthError(res);
   }
 
   req.user = payload;
